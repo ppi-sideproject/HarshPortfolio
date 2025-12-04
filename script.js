4bcb4d411031a9e690a8fb5b6f7cefe5bb7b3123
HarshVhkm <harshvhkm123@gmail.com>
2025-12-04 12:56:48 +0530

    added new

---
+++ script.js
@@ -0,0 +1,94 @@
+
+// All Variables
+// let brand = "Mercedes";
+// const model = "C-Class";
+// var year = 2021;
+// console.log(brand,model, year);
+
+
+
+
+
+// function isEven that returns true/false
+// function isEven( number ){
+//     if(number % 2 === 0){
+//        return true;
+//     }
+//     else{
+//        return false;
+//  }
+// }
+// console.log(isEven(4));
+
+
+
+// array of 5 movies and print first & last
+// let nolan = ["Tenet", "Interstellar", "Inception", "Dunkirk", "The Dark Knight"];
+// console.log(nolan[0],nolan[nolan.length-1]);
+// // nolan.push("Memento");
+// // nolan.pop();
+// // nolan.splice(2,1);
+// // nolan.shift();
+// // nolan.unshift("Following");
+// // nolan.includes("Dunkirk");
+// for(let i = 0; i < nolan.length; i++){
+//     console.log(nolan[i]);
+// }
+
+
+
+
+
+
+
+// object car with model, year, and price
+// const car={
+//     "brand": "Mercedes",
+//     "model ": "C-Class",
+//     "year": 2021,
+//     "color":"White",
+//     "price": 1000000,
+// };
+// car.terminally= "4MATIC";
+// console.log(car);
+
+// car.brand="BMW";
+// car.model="X5";
+// car.year=2022;
+// car.color="Black";
+// car.price=1500000;
+// console.log(car);
+
+
+
+
+
+// loop that prints numbers 1 to 50
+// for (let i = 1; i < 50; i++){
+//     console.log(i);
+// }
+
+//uppercase to lowercase
+// let a = "Hasrhhhhhhhhhhhvhhvi";
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// console.log(a.replace("r","p"));
+// console.log(a.replaceAll("h","k"));
+// console.log(a.indexOf("v"));
+
+// let nav = document.getElementById("h1")
+// nav.addEventListener("click",() =>{
+//    console.log("Clicked");
+// });
+
+// let nav = document.getElementById("h1")
+// nav.addEventListener("click",() =>{
+//    nav.textContent=("Unlock Technologies");
+// });
+
+// ??let input = document.getElementById(username).addEventListener("input", () =>{
+//       console.log("My Self", input.value) 
+        
+// });
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log(a.slice(0,5));
+// console.log(a.replace("r","p"));
+// console.log(a.replaceAll("h","k"));
